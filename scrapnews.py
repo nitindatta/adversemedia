601f7345f9a00e6286faef134bea80dca262de12
nitindatta <nitin.datta@gmail.com>
2020-07-07 18:12:33 +0530

    newsscrapping

---
+++ scrapnews.py
@@ -0,0 +1,91 @@
+from bs4 import BeautifulSoup
+from readability import Document
+import urllib
+import requests
+from urllib.parse import urlparse
+from extractarticle import ExtractArticle
+import re
+import logging
+from requests.exceptions import Timeout
+
+IGNORE_LIST = r'wikipedia.org|books.google|linkedin|monster|jobstreet.com|wisdomjobs|naukari|sec.gov|sec.report|dl.bourse|fintel.io|ftp.cs.princeton.edu'
+IGNORE_LINKS_LIST = r'Annual|Financial-Report'
+# desktop user-agent
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
+# mobile user-agent
+MOBILE_USER_AGENT = "Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36"
+
+GOOGLE_CLASS='gG0TJc'
+class ScrapNews:
+    links=[]
+    link=''
+    errorcount=0
+    ingnorelist=re.compile(IGNORE_LIST)
+    ingnorelink=re.compile(IGNORE_LINKS_LIST)
+    logger = logging.getLogger(__name__)
+    def __init__(self):
+        #logger = logging.getLogger(__name__)
+        print (__name__)
+        #print(logger)
+
+    def scrapEntity(self,id,pageno,keyword):
+        
+        #query =  keyword +'+"' +entity +'"'
+        #query = query.replace(' ', '+')
+        print(str(pageno))
+        query="{0}&sxsrf=ALeKk03SaWrEuh1vZDxLQXk1bJUIwMSzIA:1594109560695&source=lnms&tbm=nws&sa=X&ved=2ahUKEwjaluTw2LrqAhXb8XMBHempA28Q_AUoAXoECBQQAw&biw=1280&bih=578&start={1}".format(keyword,str(pageno))
+        URL = f"https://google.com/search?q={query}"
+        #ingnorelist=('.wikipedia.org','.books.google.*')
+        print("running query=" +URL)
+        headers = {"user-agent": USER_AGENT}
+        resp = requests.get(URL, headers=headers,timeout=3)            
+        
+        if resp.status_code == 200:
+            soup = BeautifulSoup(resp.content,features="lxml")    
+
+            results = []
+            #check for trusted new site and implement custom logic for parsing.
+            # for rest use library to get text as required.
+            
+            for g in soup.find_all('div', class_='gG0TJc'):
+                anchors = g.find_all('a')
+                if anchors:            
+                    link = anchors[0]['href']
+                    parsed_uri = urlparse(link)
+                    #optimize further to handle ignore url's
+                    if not(link in self.links
+                        or parsed_uri.path.lower().endswith('.pdf') 
+                        or self.ingnorelist.search(parsed_uri.netloc.lower()) 
+                        or link.lower().endswith('pdf')
+                        or self.ingnorelink.search(link)):
+                        extractarticle = ExtractArticle()
+                        print ('--------Extracting Link: '+ link)
+                        try:
+                            article = extractarticle.extractArticle(link)
+                            if (article is None):
+                                self.errorcount=self.errorcount+1
+                                self.logger.error("Error Processing Article for entity %s and link %s",pageno,link)
+                                continue
+                        except (requests.ConnectionError,requests.ConnectTimeout,requests.ReadTimeout):
+                            self.errorcount=self.errorcount+1
+                            self.logger.error("Connection Error While processing %s and link %s",pageno,link)
+                        else:       
+                            extractedSummary=article.summary
+                            extractedTitle=article.title
+                            summarytext = BeautifulSoup(extractedSummary,features="lxml").get_text().rstrip("\n")
+                                       
+                            title = g.find('h3').text
+                            item = {
+                                "title": title,
+                                "link": link
+                            }
+                            self.links.append(link)
+                            yield [id,pageno,keyword.strip('"'),link,extractedTitle, summarytext]
+        else:
+            self.errorcount=self.errorcount+1
+            self.logger.error("Non 200 response Error Processing %s and link %s",pageno,URL)
+            return 'error'
+    def scrapEntitykeywordList(self,id,pageno,keywords):
+        for key in keywords:
+            for o in self.scrapEntity(id,pageno,key) :
+                yield o
---
+++ searchnews.py
@@ -0,0 +1,105 @@
+from datetime import datetime
+import csv
+import os
+from scrapnews import ScrapNews
+import re
+import requests
+from requests.exceptions import Timeout,ReadTimeout
+import logging
+import argparse
+import math
+def start_scrapping():
+    print('started processing')
+    #keywords = ['"money laundering"','"market abuse" OR "market manipulation"','"insider trading"','"regulatory breach"','tax evasion','bribery OR smuggling or fraud or illegal or extortion'] # dynamic input, how ?
+    keywords = ['money laundering'] # dynamic input, how ?
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-f", "--foperand", required=False,help="file rollover")
+    ap.add_argument("-p", "--soperand", required=False,help="pages to be processed")
+    ap.add_argument("-c", "--checkpoint", required=False,help="checkpoint to continue from")
+    ap.add_argument("-m", "--filemode", required=False,help="file mode for writing")
+
+    args = vars(ap.parse_args())
+    
+    if (args['foperand']):
+        print('filerollover:' + args['foperand'])
+        filerollover=int(args['foperand'])
+    else:
+        filerollover=10
+
+    if (args['soperand']):
+        print('pagestobeprocessed:' + args['soperand'])
+        pagestobeprocessed=int(args['soperand'])
+    else:
+        pagestobeprocessed=1
+    
+    if (args['checkpoint']):
+        print('entitytobeprocessed:' + args['checkpoint'])
+        resumecheckpoint=int(args['checkpoint'])
+    else:
+        resumecheckpoint=0
+
+    if (args['filemode']):
+        print('filemode:' + args['filemode'])
+        filemode=args['filemode']
+    else:
+        filemode='w'
+
+    #print(str(resumecheckpoint/entitytobeprocessed))
+    
+    if (resumecheckpoint>0):
+        
+        filecounter=math.ceil(resumecheckpoint/pagestobeprocessed)-1
+        #filename = 'data/articleextractnews{0}.csv'.format(filecounter)       
+        #writer = csv.writer(open(filename, "a", encoding="utf-8"))
+        pagestobeprocessed = pagestobeprocessed+resumecheckpoint
+    
+    id=0
+    checkpoint=0
+    filename=''
+    #writer = csv.writer(open(filename, "w", encoding="utf-8"))
+    checkpointwriter=open("checkpoint.txt", "w")
+    scrap = ScrapNews()
+
+    #with open('data/input.csv') as csv_file:    
+        #reader = csv.DictReader(csv_file, delimiter=',')
+    for pageno in range(resumecheckpoint*10, pagestobeprocessed*10,10):
+        id=id+1
+        #entity=row['entity']
+     
+        print('-------Total pages Processed: ' + str(pageno))
+        try:
+            #if (pageno==pagestobeprocessed):
+            #    break
+            scrapedentity=scrap.scrapEntitykeywordList(id,pageno,keywords)
+
+            if ((pageno/10)%filerollover==0):
+                filename = 'data/articleextractnews{0}.csv'.format(int(int(pageno/filerollover)/10))
+                print(filename)
+                writer = csv.writer(open(filename, filemode, encoding="utf-8"))
+                writer.writerow(['id','pageno','keyword','link','title', 'summarytext'])                    
+            if (scrapedentity!='error'):
+                writer.writerows(scrapedentity)
+
+        except (requests.ConnectionError,requests.ConnectTimeout,requests.ReadTimeout):
+            print("error occured while processing: " + str(id))
+        print('-------End Processing: ' + str(pageno))            
+        #entitycount=entitycount+1
+        checkpoint=pageno
+        checkpointwriter.write(str(checkpoint))
+    checkpointwriter.close()
+    print('-------Total pages Processed: ' + str(id) + 
+    ' Total Errors:' + str(scrap.errorcount))
+    logging.error('-------Total pages Processed: ' + str(id) + 
+    ' Total Errors:' + str(scrap.errorcount))
+
+logging.basicConfig(filename='app.log',filemode='w')
+start_scrapping()
+#keywords = ['"money laundering"']
+#print(keywords[0].strip('"'))
+#ingnorelist=re.compile(r'Annual')
+#if not(ingnorelist.search('https://crdbbank.co.tz/wp-content/uploads/2019/05/Annual%20Report%202018.pdf')):
+#    print('u')
+#ingnorelist=re.compile(r'text/html')
+#print(re.search(r'text/html','text/html; charset=utf-8'))
+#resp = requests.get("https://internationalbanker.com/banking/commercial-bank-kuwait-convert-islamic-banking/",timeout=3)
+#print(resp.text)
